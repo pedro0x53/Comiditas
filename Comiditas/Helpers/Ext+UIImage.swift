ff732225b5623d9df911fc8a6a8f10dddb2f2786
Pedro Sousa <pedrosousa2708@gmail.com>
2021-09-20 09:09:07 -0300

    remote image loader fuction added to uiimage view extension

--- Comiditas/Helpers/Ext+UIImage.swift
+++ Comiditas/Helpers/Ext+UIImage.swift
@@ -17,3 +17,17 @@ extension UIImage {
         self.init(named: assetIdentifier.rawValue)
     }
 }
+
+// Remote image loader
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url),
+               let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            }
+        }
+    }
+}
