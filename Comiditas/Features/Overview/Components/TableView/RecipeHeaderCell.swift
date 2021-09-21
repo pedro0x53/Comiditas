a72f549939f4002a2da5c4241de428e79dfb61b9
Pedro Sousa <pedrosousa2708@gmail.com>
2021-09-21 17:30:21 -0300

    Recipe Overview semi-finished

---
+++ Comiditas/Features/Overview/Components/TableView/RecipeHeaderCell.swift
@@ -0,0 +1,127 @@
+//
+//  RecipeHeaderCell.swift
+//  Comiditas
+//
+//  Created by Pedro Sousa on 20/09/21.
+//
+// swiftlint:disable function_parameter_count
+
+import UIKit
+
+class RecipeHeaderCell: UITableViewCell, BaseViewProtocol {
+
+    public static let identifier: String = "RecipeHeaderCellIdentifier"
+
+    private let mainImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Colors.primary
+        label.font = Fonts.h3
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let servingsLabel: UILabel = {
+        let label = UILabel()
+        label.font = Fonts.h6
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let detailsStack: UIStackView = {
+        let stack = UIStackView()
+        stack.alignment = .center
+        stack.axis = .horizontal
+        stack.distribution = .equalSpacing
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+
+    private let prepTimeItem: DetailView = DetailView(iconName: "clock.fill", description: "")
+    private let difficultyItem: DetailView = DetailView(iconName: "flame.fill", description: "")
+    private let rateItem: DetailView = DetailView(iconName: "star.fill", description: "")
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupView() {
+        contentView.addSubview(mainImage)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(servingsLabel)
+        contentView.addSubview(detailsStack)
+
+        detailsStack.addArrangedSubview(prepTimeItem)
+        detailsStack.addArrangedSubview(difficultyItem)
+        detailsStack.addArrangedSubview(rateItem)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mainImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mainImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            mainImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            mainImage.heightAnchor.constraint(equalToConstant: 195),
+
+            titleLabel.topAnchor.constraint(equalTo: mainImage.bottomAnchor, constant: 24),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+
+            servingsLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            servingsLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            servingsLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+
+            detailsStack.topAnchor.constraint(equalTo: servingsLabel.bottomAnchor, constant: 24),
+            detailsStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 32),
+            detailsStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -32),
+            detailsStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        ])
+    }
+
+    func configure(imageURL: String, title: String, servings: String,
+                   prepTime: String, difficulty: String, rating: Int) {
+        if let url = URL(string: imageURL) {
+            self.mainImage.load(url: url)
+        }
+        self.mainImage.accessibilityLabel = "Foto do prato finalizado."
+
+        self.titleLabel.text = title
+        self.titleLabel.accessibilityLabel = title
+
+        self.servingsLabel.text = "Rendimento: " + servings
+        self.servingsLabel.accessibilityLabel = "Rendimento de " + servings
+
+        self.prepTimeItem.itemDescription = prepTime
+        self.prepTimeItem.accessibilityLabel = "Tempo de preparo: " + prepTime + "minutos."
+
+        self.difficultyItem.itemDescription = difficulty
+        self.difficultyItem.accessibilityLabel = "Dificuldade: fácil."
+
+        self.rateItem.itemDescription = "\(rating)"
+        let stars = ((rating > 1) ? "estrelas" : "estrela") + "."
+        self.rateItem.accessibilityLabel = "Avaliação de \(rating)" + stars
+
+        self.accessibilityElements = [
+            mainImage,
+            titleLabel,
+            servingsLabel,
+            prepTimeItem,
+            difficultyItem,
+            rateItem
+        ]
+    }
+}
---
+++ Comiditas/Features/Overview/OverviewViewController.swift
@@ -0,0 +1,139 @@
+//
+//  OverviewViewController.swift
+//  Comiditas
+//
+//  Created by Pedro Sousa on 21/09/21.
+//
+
+import UIKit
+
+class OverviewViewController: UIViewController {
+
+    weak var coordinator: OverviewCoordinator?
+    let associatedView: OverviewView = OverviewView()
+
+    var recipe: RecipesJson
+
+    init(recipe: RecipesJson) {
+        self.recipe = recipe
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        super.loadView()
+        associatedView.delegate = self
+        self.view = associatedView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension OverviewViewController: OverviewViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = OverviewSection(rawValue: section) else { return 0 }
+
+        switch section {
+        case .header:
+            return 1
+        case .ingredients:
+            return recipe.ingredients.count
+        case .steps:
+            return recipe.steps.count
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let section = OverviewSection(rawValue: indexPath.section) else { return UITableViewCell() }
+
+        switch section {
+        case .header:
+            guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: RecipeHeaderCell.identifier) as? RecipeHeaderCell
+            else {
+                return UITableViewCell()
+            }
+
+            cell.configure(imageURL: recipe.imageURL, title: recipe.name,
+                           servings: "\(recipe.servings) porções", prepTime: "\(recipe.prepTime)",
+                           difficulty: "fácil", rating: recipe.rate)
+
+            return cell
+
+        case .ingredients:
+            let ingredient = "\(recipe.ingredients[indexPath.row])"
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+            cell.textLabel?.text = "• " + ingredient
+            cell.accessibilityLabel = ingredient
+            return cell
+
+        case .steps:
+            guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: RecipeStepCell.identifier) as? RecipeStepCell
+            else {
+                return UITableViewCell()
+            }
+
+            cell.configure(title: "Passo \(indexPath.row + 1)",
+                           description: recipe.steps[indexPath.row].stepDescription,
+                           finished: (indexPath.row == 0) ? true : false)
+
+            return cell
+        }
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let section = OverviewSection(rawValue: section) else { return nil }
+
+        switch section {
+        case .header:
+            return nil
+        case .ingredients, .steps:
+            let title: String = (section == .ingredients) ? "Ingedientes" : "Modo de Preparo"
+            guard let header = tableView.dequeueReusableHeaderFooterView(
+                    withIdentifier: SectionHeaderView.identifier) as? SectionHeaderView
+            else {
+                let header = SectionHeaderView()
+                header.configure(title: title)
+                return header
+            }
+
+            header.configure(title: title)
+            return header
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        guard let section = OverviewSection(rawValue: section) else { return 0 }
+        switch section {
+        case .header:
+            return 0
+        case .ingredients, .steps:
+            return 44
+        }
+    }
+}
+
+extension OverviewViewController {
+    enum OverviewSection: Int {
+        case header = 0
+        case ingredients = 1
+        case steps = 2
+    }
+}
+
+extension OverviewViewController {
+    func startRecipe() {
+        coordinator?.coordinateToSteps(recipe: self.recipe)
+    }
+}
