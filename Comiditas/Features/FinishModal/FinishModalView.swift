79e71488f8bb40368ccc498c5872af8b2d5576e6
Brena Amorim <brena.amorim@live.com>
2021-09-21 20:56:28 -0300

    Create Finish View

---
+++ Comiditas/Features/FinishModal/FinishModalView.swift
@@ -0,0 +1,20 @@
+//
+//  FinishModalView.swift
+//  Comiditas
+//
+//  Created by Brena Amorim on 21/09/21.
+//
+
+import UIKit
+
+class FinishModalView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
