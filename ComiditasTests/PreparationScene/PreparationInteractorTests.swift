0d46fb4d3c60feaa9a44cf6a1d4dc55ce23b9327
All3yp <noyalle@ualberta.ca>
2021-10-04 09:02:49 -0300

    Create unit tests on PresentationView Interactor and Presenter

--- ComiditasTests/PreparationScene/PreparationInteractorTests.swift
+++ ComiditasTests/PreparationScene/PreparationInteractorTests.swift
@@ -10,4 +10,54 @@ import XCTest
 
 class PreparationInteractorTests: XCTestCase {
 
+    // MARK: Subject under test
+
+    var sut: PreparationInteractor!
+
+    override func setUp() {
+        super.setUp()
+        setupPreparationInteractor()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    // MARK: Test setup
+
+    func setupPreparationInteractor() {
+        sut = PreparationInteractor()
+    }
+
+    // MARK: Test doubles
+
+    class PreparationPresentationLogicSpy: PreparationPresentationLogic {
+
+        // MARK: Method call expectations
+
+        var presentStepsCalled = false
+        var presentNextStepCalled = false
+        var presentPreviousStepCalled = false
+        var presentFinishedCalled = false
+
+        // MARK: Spied methods
+
+        func presentSteps(response: PreparationModels.GetSteps.Response) {
+            presentStepsCalled = true
+        }
+
+        func presentNextStep(response: PreparationModels.NextStep.Response) {
+            presentNextStepCalled = true
+        }
+
+        func presentPreviousStep(response: PreparationModels.PreviousStep.Response) {
+            presentPreviousStepCalled = true
+        }
+
+        func presentFinished(response: PreparationModels.Finished.Response) {
+            presentFinishedCalled = true
+        }
+
+        // MARK: - Tests
+    }
 }
--- ComiditasTests/PreparationScene/PreparationPresenterTests.swift
+++ ComiditasTests/PreparationScene/PreparationPresenterTests.swift
@@ -10,4 +10,63 @@ import XCTest
 
 class PreparationPresenterTests: XCTestCase {
 
+    // MARK: - Subject under test
+
+    var sut: PreparationPresenter!
+
+    // MARK: - Test lifecycle
+
+    override func setUp() {
+      super.setUp()
+      setupShowPreparationPresenter()
+    }
+
+    override func tearDown() {
+      super.tearDown()
+    }
+
+    // MARK: - Test setup
+
+    func setupShowPreparationPresenter() {
+      sut = PreparationPresenter()
+    }
+
+    class PreparationDisplayLogicSpy: PreparationDisplayLogic {
+
+        // MARK: Method call expectations
+
+        var displayStepsCalled = false
+        var displayNextStepCalled = false
+        var displayPreviousStepCalled = false
+        var displayFinishedCalled = false
+
+        // MARK: Argument expectations
+
+        var viewModelGetSteps: PreparationModels.GetSteps.ViewModel!
+        var viewModelNextStep: PreparationModels.NextStep.ViewModel!
+        var viewModelPreviousStep: PreparationModels.PreviousStep.ViewModel!
+        var viewModelFinished: PreparationModels.Finished.ViewModel!
+
+        func displaySteps(viewModel: PreparationModels.GetSteps.ViewModel) {
+            displayStepsCalled = true
+            self.viewModelGetSteps = viewModel
+        }
+
+        func displayNextStep(viewModel: PreparationModels.NextStep.ViewModel) {
+            displayNextStepCalled = true
+            self.viewModelNextStep = viewModel
+        }
+
+        func displayPreviousStep(viewModel: PreparationModels.PreviousStep.ViewModel) {
+            displayPreviousStepCalled = true
+            self.viewModelPreviousStep = viewModel
+        }
+
+        func displayFinished(viewModel: PreparationModels.Finished.ViewModel) {
+            displayFinishedCalled = true
+            self.viewModelFinished = viewModel
+        }
+
+        // MARK: - Tests
+    }
 }
