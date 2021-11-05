10bee214973a3554398ca7a256d4a29f72595ae0
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2021-11-05 11:21:32 -0300

    search

--- Comiditas/Features/Search/View/SearchView.swift
+++ Comiditas/Features/Search/View/SearchView.swift
@@ -20,13 +20,43 @@ class SearchView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    lazy var searchBar: UISearchBar = {
+        let search = UISearchBar()
+        search.placeholder = FeedLocalizable.search.text
+        search.sizeToFit()
+        search.isTranslucent = false
+        search.translatesAutoresizingMaskIntoConstraints = false
+        search.backgroundColor = Colors.background
+        search.searchBarStyle = .minimal
+        return search
+    }()
+
+    lazy var tagsView: TagsView = {
+        let view = TagsView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 }
 
 extension SearchView: BaseViewProtocol {
     func setupView() {
         self.backgroundColor = Colors.background
+        self.addSubview(searchBar)
+        self.addSubview(tagsView)
         setupConstraints()
     }
 
-    func setupConstraints() {}
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0),
+            searchBar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            searchBar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+
+            tagsView.topAnchor.constraint(equalTo: self.searchBar.bottomAnchor, constant: 16),
+            tagsView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            tagsView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            tagsView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
--- Comiditas/Features/Search/View/SearchViewController.swift
+++ Comiditas/Features/Search/View/SearchViewController.swift
@@ -14,9 +14,16 @@ class SearchViewController: UIViewController {
     let contentView = SearchView(frame: UIScreen.main.bounds)
     var interactor: SearchInteractorProtocol?
 
+    let headers = ["Populares", "Outros"]
+    let popular = ["Massas", "Rápidas", "Vegetariano", "Doces"]
+    let others = ["Bolos e tortas", "Frutos do mar", "Carnes", "Peixes",
+                  "Saladas", "Aves", "Vegano", "Lacfree", "Sopas",
+                  "Molhos", "Bebidas", "Glúten free", "Fit", "Fáceis"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupVIP()
+        setupTableView()
     }
 
     override func loadView() {
@@ -34,6 +41,77 @@ class SearchViewController: UIViewController {
         viewController.interactor = interactor
         interactor.presenter = presenter
         presenter.viewController = viewController
+    }
+
+    func setupTableView() {
+        contentView.tagsView.collectionView.delegate = self
+        contentView.tagsView.collectionView.dataSource = self
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate,
+                                UICollectionViewDataSource,
+                                UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == .zero {
+            return popular.count
+        }
+
+        return others.count
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "TagsCollectionViewCell",
+            for: indexPath
+        ) as? TagsCollectionViewCell else { return UICollectionViewCell() }
+
+        cell.label.text = (indexPath.section == .zero) ? popular[indexPath.row] : others[indexPath.row]
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == .zero {
+            return CGSize(
+                width: popular[indexPath.row].size(withAttributes: [NSAttributedString.Key.font: Fonts.h5]).width + 13,
+                height: 40
+            )
+        }
+        return CGSize(
+            width: others[indexPath.row].size(withAttributes: [NSAttributedString.Key.font: Fonts.h5]).width + 13,
+            height: 40
+        )
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let headerView = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: "TagsHeaderCollectionReusableView",
+            for: indexPath
+        ) as? TagsHeaderCollectionReusableView else {
+            return UICollectionReusableView()
+        }
+        headerView.headerLabel.text = headers[indexPath.section]
+        return headerView
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 40)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("clicou")
     }
 }
 
