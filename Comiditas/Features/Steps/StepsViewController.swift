0619e179696e054a2ed32570bbb8a18dbae865dc
All3yp <noyalle@ualberta.ca>
2021-11-23 10:36:15 -0300

    Add Accessibility order infoButton

--- Comiditas/Features/Steps/StepsViewController.swift
+++ Comiditas/Features/Steps/StepsViewController.swift
@@ -290,7 +290,8 @@ extension StepsViewController {
             stepsView.nextStepView,
             stepsView.stackView.previousButton,
             stepsView.stackView.nextButton,
-            stepsView.closeButton
+            stepsView.closeButton,
+            stepsView.infoButton
         ]
         UIAccessibility.post(notification: .layoutChanged, argument: stepsView.stackView.bottomLabelsStackView)
     }
